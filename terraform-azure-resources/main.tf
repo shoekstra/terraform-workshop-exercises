28b58dc7d70314bcfb762ec5754c4bee8f86738a
Stephen Hoekstra <shoekstra@schubergphilis.com>
2024-10-13 22:21:57 +0200

    Add `terraform-azure-resources`
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

---
+++ terraform-azure-resources/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+
+  required_version = ">= 1.1.0" # Set this to require a minimum Terraform version
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_string" "example" {
+  length  = 4
+  special = false
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "myTFResourceGroup-${random_string.example.result}"
+  location = "westus2"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacct-${random_string.example.result}"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
